a2ff52f3165ce88f540202625251619391ea702d
Zath <sathyr2003@gmail.com>
2026-06-08 21:42:20 -0300

    feat: se crean agentes

--- Proyecto08062026/agentes.py
+++ Proyecto08062026/agentes.py
@@ -1,1 +1,4 @@
+#Sistema de Agentes de IA - Reviews de Amazon 
+#Instalación de dependencias necesarias para el proyecto
+!pip install kagglehub pandas numpy matplotlib seaborn scikit-learn nltk wordcloud
 
